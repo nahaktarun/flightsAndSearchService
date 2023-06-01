1c7205d1c66a3bf5f976b72e2c48e8f75d810284
nahaktarun <tarunnahak25@gmail.com>
2023-06-01 20:29:57 +0530

    Added - city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,27 +1,44 @@
-const {City} = require('../models/index');
+const { City } = require("../models/index");
 
 class CityRepository {
-
-    async createCity({name}){
-        try{
-            const city = await City.Create({name});
-            return city;
-        }catch(error){
-            throw {error};
-        }
+  async createCity({ name }) {
+    try {
+      const city = await City.Create({ name });
+      return city;
+    } catch (error) {
+      throw { error };
     }
+  }
 
-    async deleteCity({cityId}){
-        try{
-            await City.destroy({
-                where: {
-                    id: cityId
-                }
-            })
-        }catch(error){
-            throw {error};
-        }
+  async deleteCity({ cityId }) {
+    try {
+      await City.destroy({
+        where: {
+          id: cityId,
+        },
+      });
+    } catch (error) {
+      throw { error };
     }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Somethign went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    const city = await City.update(data, {
+      where: {
+        id: cityId,
+      },
+    });
+  }
 }
 
 module.exports = CityRepository;
